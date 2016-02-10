fde4f0a3b29932cd6cd6159de52cde8f2253fb27
Dan Bahena <bahena@pythian.com>
2016-02-09 21:42:38 -0600

    Agregando comentarios

--- game.py
+++ game.py
@@ -3,6 +3,8 @@ import RPi.GPIO as GPIO
 import time
 import random
 
+#Necesito modificar los strings que solicitan el input del usuario, en vez de "left player" usar "jugador izquierdo"? así se lee bien?
+
 left_name = input('left player name is ')
 right_name = input('right player name is ')
 
