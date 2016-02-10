cbd4e7a7e1837b35b975fa4b5e7865a5f1c791d0
Dan Bahena <bahena@pythian.com>
2016-02-09 21:38:00 -0600

    Commit inicial

---
+++ game.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+import random
+
+left_name = input('left player name is ')
+right_name = input('right player name is ')
+
+names = [left_name, right_name]
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+led = 4
+right_button = 15
+left_button = 14
+
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(right_button, GPIO.IN, GPIO,PUD_UP)
+GPIO.setup(left_button, GPIO.IN, GPIO,PUD_UP)
+
+
+GPIO.output(led, 1)
+time.sleep(random.uniform(5, 10))
+GPIO.output(led, 0)
+
+while True:
+    if GPIO.input(left_button) == False:
+        print (names [0] + " gano")
+        break
+    if GPIO.input(right_button) == False:
+        print (names [1] + " gano")
+        break
+
+GPIO.cleanup()
